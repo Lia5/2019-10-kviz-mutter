fd578d8134e440bbba6d3a94c5ed23e104071a05
Helen <liaforweb@gmail.com>
2019-10-26 19:16:26 +0300

    mob touch

--- myapp/assets/js/custom.js
+++ myapp/assets/js/custom.js
@@ -634,6 +634,36 @@ $(function() {
   }
 
 
+  //mob
+  if ( window.innerWidth < 1023 || window.screen.width < 1023) {
+    // find the element that you want to drag.
+      var box = document.getElementById('question1__word15');
+      
+      /* listen to the touchMove event,
+      every time it fires, grab the location
+      of touch and assign it to box */
+      
+      box.addEventListener('touchmove', function(e) {
+        // grab the location of touch
+        var touchLocation = e.targetTouches[0];
+        box.style = "position: absolute";
+        // assign box new coordinates based on the touch.
+        box.style.left = touchLocation.pageX + 'px';
+        box.style.top = touchLocation.pageY + 'px';
+      })
+      
+      /* record the position of the touch
+      when released using touchend event.
+      This will be the drop position. */
+      
+      box.addEventListener('touchend', function(e) {
+        // current box position.
+        var x = parseInt(box.style.left);
+        var y = parseInt(box.style.top);
+      })
+  
+  }
+
 });
 
 
